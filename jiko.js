016517f9e2fffddbddb96637a5f12c30f305d8ef
yoovin.shim <soo22839@gmail.com>
2018-06-21 14:37:33 +0900

    15
    save

--- jiko.js
+++ jiko.js
@@ -1,3 +1,17 @@
 function changebackground() {
     document.getElementById("asd").style.backgroundColor = "yellow";
 }
+function myMove() {
+    var elem = document.getElementById("animete");
+    var pos = 0;
+    var id = setInterval (frame, 5);
+    function frame() {
+        if (pos == 350) {
+            clearInterval (id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
